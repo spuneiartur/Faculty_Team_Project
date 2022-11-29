88bb28c06cfeb5426229831c5f579c0b0e12cfb4
Cristian Stan <stancristian2002@gmail.com>
2022-11-29 18:20:14 +0200

    Updated version of lexer for select

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -44,9 +44,8 @@ int main()
 {
 	int sizeOfTokenizedVector, i = 0;
 	char** tokenizedVector = tokenizingFunction("CREATE TABLE students ((id, integer, 1000, 0), (nume, text, 128, ''), (nume, text, 16, '10'))", sizeOfTokenizedVector, ' ');
-	
+	//char** tokenizedVector = tokenizingFunction("SELECT (table alpha) FROM spain", sizeOfTokenizedVector, ' ');
 	int* vectorTypeOfToken = identifyKeywordTypeVector(tokenizedVector, sizeOfTokenizedVector);
-
 	Token token(tokenizedVector, sizeOfTokenizedVector, vectorTypeOfToken);
 
 	token.getVectorTypeOfTokenValues();
