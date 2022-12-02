f4b7cc9f596bf3adf9d625020797b287967494b5
Spunei Artur <arthurspunei@gmail.com>
2022-12-02 12:20:50 +0200

    deleted comments

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -15,14 +15,7 @@ int main()
 	std::string commandLine = " CREATE TABLE Studenteel IF NOT EXISTS ((id, integer, 1000, 0), (nume, text, 128, ''), (grupa, text, 16, '10')); CREATE TABLE students ((id, integer, 1000, 0), (nume, text, 128, ''), (nume, text, 16, '10')); drop index rgrg; Display table studenteel;";
 	
 	loopingThroughCommands(commandLine);
-	/*int maxSize[] = { 3, 128/8, 50/10 };
-	int i1 = 3;
-	int i2 = 128/8 ;
-	int i3 = 3;
-	printf("%-*s| %-*s| %-*s|\n",i1 + 10, "id", i2+ 10,  "name", i3 + 10,  "group");
-	for (int i = 0; i < i1 + i3 + i2 + 30; i++) printf("-");
-	printf("\n\n\n\n\n\n");*/
-	
+
 
 	return 0;
 }
